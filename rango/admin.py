831146d3089ba63a20befce17833376430005efe
Tiffany Babey <232887b@student.gla.ac.uk>
2019-01-29 15:13:36 +0000

    Ch5.end

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, Question, Choice
 
 admin.site.register(Category)
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url')
+	
+admin.site.register(Page, PageAdmin)
+
+
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+	
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [(None, {'fields': ['question_text']}), ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
+	
+admin.site.register(Question, QuestionAdmin)
+
+
